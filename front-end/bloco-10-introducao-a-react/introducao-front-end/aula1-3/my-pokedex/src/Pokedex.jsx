0e3b975b13db95fa3b2567af0b12b79339625e45
MateusLucas <mateus.some.ms@gmail.com>
2022-03-05 00:00:26 -0300

    criei componente pokemon e pokedex

---
+++ front-end/bloco-10-introducao-a-react/introducao-front-end/aula1-3/my-pokedex/src/Pokedex.jsx
@@ -0,0 +1,12 @@
+import { Component } from 'react'
+import Pokemon from './Pokemon.';
+
+class Pokedex extends Component {
+  render() {
+    return(
+      <Pokemon />
+    )
+  }
+}
+
+export default Pokedex;
